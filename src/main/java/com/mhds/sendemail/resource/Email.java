7af8a20e63632621223bc2b80f1e4a0955cde923
wallace.silva <gomesw@gmail.com>
2024-10-10 21:30:27 -0300

    in docker hub

---
+++ src/main/java/com/mhds/sendemail/resource/Email.java
@@ -0,0 +1,15 @@
+package com.mhds.sendemail.resource;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Email {
+
+    public ResponseEntity<?> send(){
+        return ResponseEntity.ok("Enviado comsucesso");
+    }
+
+}
