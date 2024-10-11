5cea089ed6188ecd33e73fc64e1af60f1d709aea
wallace.silva <gomesw@gmail.com>
2024-10-10 21:34:51 -0300

    resource

--- src/main/java/com/mhds/sendemail/resource/Email.java
+++ src/main/java/com/mhds/sendemail/resource/Email.java
@@ -1,6 +1,7 @@
 package com.mhds.sendemail.resource;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class Email {
 
+    @GetMapping("/send")
     public ResponseEntity<?> send(){
         return ResponseEntity.ok("Enviado comsucesso");
     }
